8deeb4b7b88fbad8dea656395d3efbf6414aec4a
Vibhanshu <vrana055@gmail.com>
2022-05-16 23:27:31 +0530

    bar - chart

---
+++ d3-chart/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import BarChart from './charts/Chart';
+
+function App() {
+    return (
+        <div className="App">
+          <BarChart />
+        </div>
+    );
+}
+
+export default App;
